64e01a952e2171bb71dcd5bfc49e75a5c05e95f7
dcuoliveira <dan.cunhaa@gmail.com>
2024-01-15 21:14:21 -0300

    initialize bootstrap class only

--- src/gym/naive.py
+++ src/gym/naive.py
@@ -58,8 +58,8 @@ class training_etfstsm(TSM, DependentBootstrapSampling, Functionals):
                                             time_series=torch.tensor(self.returns_info.to_numpy()),
                                             boot_method=boot_method,
                                             Bsize=Bsize)
-        self.all_samples = self.sample_many_paths(k=k)
-        self.n_bootstrap_samples = self.all_samples.shape[0]
+        # self.all_samples = self.sample_many_paths(k=k)
+        # self.n_bootstrap_samples = self.all_samples.shape[0]
 
     def build_returns(self):
         returns = []
