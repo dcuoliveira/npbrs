f2d7a4ec0bf309d39fc9d794481f3188e857908d
dcuoliveira <dan.cunhaa@gmail.com>
2024-01-16 07:34:25 -0300

    comment sys path

--- src/gym/training_etfstsm.py
+++ src/gym/training_etfstsm.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+# sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 import pandas as pd
 import numpy as np
