3963258038958735a6074a1462a68daddc3cda9e
dcuoliveira <dan.cunhaa@gmail.com>
2024-01-16 07:39:10 -0300

    add run

---
+++ src/run_training_etfstsm.sh
@@ -0,0 +1,9 @@
+python -gym.training_etfstsm --utility Sharpe --functional means --alpha 0.95 --k 100
+python -gym.training_etfstsm --utility AverageDD --functional means --alpha 0.95 --k 100
+python -gym.training_etfstsm --utility MaxDD --functional means --alpha 0.95 --k 100
+python -gym.training_etfstsm --utility PositiveRetRatio --functional means --alpha 0.95 --k 100
+python -gym.training_etfstsm --utility Sharpe --functional means --alpha 0.05 --k 100
+python -gym.training_etfstsm --utility AverageDD --functional means --alpha 0.05 --k 100
+python -gym.training_etfstsm --utility MaxDD --functional means --alpha 0.05 --k 100
+python -gym.training_etfstsm --utility PositiveRetRatio --functional means --alpha 0.05 --k 100
+
