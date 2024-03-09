c0662a3368526f4c2a910facc997cb5e9ff74930
dcuoliveira <dan.cunhaa@gmail.com>
2024-03-09 14:38:50 -0300

    fix percentile

--- src/run_training_etfstsm.sh
+++ src/run_training_etfstsm.sh
@@ -2,8 +2,8 @@ python -m gym.training_etfstsm --utility Sharpe --functional means --alpha 0.95 
 python -m gym.training_etfstsm --utility AvgDD --functional means --alpha 0.95 --k 100
 python -m gym.training_etfstsm --utility MaxDD --functional means --alpha 0.95 --k 100
 python -m gym.training_etfstsm --utility "% Positive Ret." --functional means --alpha 0.95 --k 100
-python -m gym.training_etfstsm --utility Sharpe --functional means --alpha 0.05 --k 100
-python -m gym.training_etfstsm --utility AvgDD --functional means --alpha 0.05 --k 100
-python -m gym.training_etfstsm --utility MaxDD --functional means --alpha 0.05 --k 100
-python -m gym.training_etfstsm --utility "% Positive Ret." --functional means --alpha 0.05 --k 100
+python -m gym.training_etfstsm --utility Sharpe --functional means --alpha 0.75 --k 100
+python -m gym.training_etfstsm --utility AvgDD --functional means --alpha 0.75 --k 100
+python -m gym.training_etfstsm --utility MaxDD --functional means --alpha 0.75 --k 100
+python -m gym.training_etfstsm --utility "% Positive Ret." --functional means --alpha 0.75 --k 100
 
