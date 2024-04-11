7e617e790661d0a51a3ee5e40907244a5f35d77e
dcuoliveira <dan.cunhaa@gmail.com>
2024-04-11 11:14:07 -0300

    add k=1k

--- src/run_training_etfstsm_sharpe.sh
+++ src/run_training_etfstsm_sharpe.sh
@@ -1,7 +1,7 @@
-python -m gym.training_etfstsm --utility Sharpe --functional means --alpha 0.95 --k 100 --end_date "2015-12-31" --cpu_count -1
-python -m gym.training_etfstsm --utility Sharpe --functional means --alpha 0.75 --k 100 --end_date "2015-12-31" --cpu_count -1
-python -m gym.training_etfstsm --utility Sharpe --functional means --alpha 0.50 --k 100 --end_date "2015-12-31" --cpu_count -1
-python -m gym.training_etfstsm --utility Sharpe --functional means --alpha 0.25 --k 100 --end_date "2015-12-31" --cpu_count -1
-python -m gym.training_etfstsm --utility Sharpe --functional means --alpha 1 --k 100 --end_date "2015-12-31" --cpu_count -1
+python -m gym.training_etfstsm --utility Sharpe --functional means --alpha 0.95 --k 1000 --end_date "2015-12-31" --cpu_count -1
+python -m gym.training_etfstsm --utility Sharpe --functional means --alpha 0.75 --k 1000 --end_date "2015-12-31" --cpu_count -1
+python -m gym.training_etfstsm --utility Sharpe --functional means --alpha 0.50 --k 1000 --end_date "2015-12-31" --cpu_count -1
+python -m gym.training_etfstsm --utility Sharpe --functional means --alpha 0.25 --k 1000 --end_date "2015-12-31" --cpu_count -1
+python -m gym.training_etfstsm --utility Sharpe --functional means --alpha 1 --k 1000 --end_date "2015-12-31" --cpu_count -1
 
 
