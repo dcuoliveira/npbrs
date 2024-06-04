b9196099d2326f3517972b50ef3d30de505a51a4
dcuoliveira <dan.cunhaa@gmail.com>
2024-06-04 07:51:47 -0300

    add alphas

--- src/run_training_etfstsm_moskowitz_maxdd.sh
+++ src/run_training_etfstsm_moskowitz_maxdd.sh
@@ -1,7 +1,7 @@
-# python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means --alpha 1 --k 1000 --end_date "2015-12-31" --cpu_count -1
-# python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means --alpha 0.90 --k 1000 --end_date "2015-12-31" --cpu_count -1
-# python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means --alpha 0.80 --k 1000 --end_date "2015-12-31" --cpu_count -1
-# python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means --alpha 0.70 --k 1000 --end_date "2015-12-31" --cpu_count -1
+python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means --alpha 1 --k 1000 --end_date "2015-12-31" --cpu_count -1
+python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means --alpha 0.90 --k 1000 --end_date "2015-12-31" --cpu_count -1
+python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means --alpha 0.80 --k 1000 --end_date "2015-12-31" --cpu_count -1
+python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means --alpha 0.70 --k 1000 --end_date "2015-12-31" --cpu_count -1
 python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means --alpha 0.60 --k 1000 --end_date "2015-12-31" --cpu_count -1
 python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means --alpha 0.50 --k 1000 --end_date "2015-12-31" --cpu_count -1
 python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means --alpha 0.40 --k 1000 --end_date "2015-12-31" --cpu_count -1
