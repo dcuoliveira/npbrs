932145876f9c4cdb89d32c8303b63103ec4d1e9b
dcuoliveira <dan.cunhaa@gmail.com>
2024-06-06 15:39:53 -0300

    update

--- src/run_training_etfstsm_cta_sharpe.sh
+++ src/run_training_etfstsm_cta_sharpe.sh
@@ -1,11 +1,11 @@
-python -m gym.training_etfstsm_cta --utility Sharpe --functional means --alpha 1 --k 1000 --end_date "2015-12-31" --cpu_count -1
-python -m gym.training_etfstsm_cta --utility Sharpe --functional means --alpha 0.90 --k 1000 --end_date "2015-12-31" --cpu_count -1
-python -m gym.training_etfstsm_cta --utility Sharpe --functional means --alpha 0.80 --k 1000 --end_date "2015-12-31" --cpu_count -1
-python -m gym.training_etfstsm_cta --utility Sharpe --functional means --alpha 0.70 --k 1000 --end_date "2015-12-31" --cpu_count -1
-python -m gym.training_etfstsm_cta --utility Sharpe --functional means --alpha 0.60 --k 1000 --end_date "2015-12-31" --cpu_count -1
-python -m gym.training_etfstsm_cta --utility Sharpe --functional means --alpha 0.50 --k 1000 --end_date "2015-12-31" --cpu_count -1
-python -m gym.training_etfstsm_cta --utility Sharpe --functional means --alpha 0.40 --k 1000 --end_date "2015-12-31" --cpu_count -1
-python -m gym.training_etfstsm_cta --utility Sharpe --functional means --alpha 0.30 --k 1000 --end_date "2015-12-31" --cpu_count -1
-python -m gym.training_etfstsm_cta --utility Sharpe --functional means --alpha 0.20 --k 1000 --end_date "2015-12-31" --cpu_count -1
-python -m gym.training_etfstsm_cta --utility Sharpe --functional means --alpha 0.10 --k 1000 --end_date "2015-12-31" --cpu_count -1
+python -m gym.training_etfstsm_cta --utility Sharpe --functional means --alpha 1 --k 1000 --train_size 0.8 --cpu_count -1
+python -m gym.training_etfstsm_cta --utility Sharpe --functional means --alpha 0.90 --k 1000 --train_size 0.8 --cpu_count -1
+python -m gym.training_etfstsm_cta --utility Sharpe --functional means --alpha 0.80 --k 1000 --train_size 0.8 --cpu_count -1
+python -m gym.training_etfstsm_cta --utility Sharpe --functional means --alpha 0.70 --k 1000 --train_size 0.8 --cpu_count -1
+python -m gym.training_etfstsm_cta --utility Sharpe --functional means --alpha 0.60 --k 1000 --train_size 0.8 --cpu_count -1
+python -m gym.training_etfstsm_cta --utility Sharpe --functional means --alpha 0.50 --k 1000 --train_size 0.8 --cpu_count -1
+python -m gym.training_etfstsm_cta --utility Sharpe --functional means --alpha 0.40 --k 1000 --train_size 0.8 --cpu_count -1
+python -m gym.training_etfstsm_cta --utility Sharpe --functional means --alpha 0.30 --k 1000 --train_size 0.8 --cpu_count -1
+python -m gym.training_etfstsm_cta --utility Sharpe --functional means --alpha 0.20 --k 1000 --train_size 0.8 --cpu_count -1
+python -m gym.training_etfstsm_cta --utility Sharpe --functional means --alpha 0.10 --k 1000 --train_size 0.8 --cpu_count -1
 
