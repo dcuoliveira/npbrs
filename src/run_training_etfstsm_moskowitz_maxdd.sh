12260e2805812175ed83b5376d5c3454affff2cf
dcuoliveira <dan.cunhaa@gmail.com>
2024-07-13 19:12:01 -0300

    fix sg

--- src/run_training_etfstsm_moskowitz_maxdd.sh
+++ src/run_training_etfstsm_moskowitz_maxdd.sh
@@ -1,4 +1,24 @@
-python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means --alpha 1 --k 100000 --train_size 0.8 --cpu_count -1
+python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means -- --alpha 1 --k 100 --train_size 0.8 --cpu_count -1
+python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means --alpha 0.90 --k 100 --train_size 0.8 --cpu_count -1
+python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means --alpha 0.80 --k 100 --train_size 0.8 --cpu_count -1
+python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means --alpha 0.70 --k 100 --train_size 0.8 --cpu_count -1
+python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means --alpha 0.60 --k 100 --train_size 0.8 --cpu_count -1
+python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means --alpha 0.50 --k 100 --train_size 0.8 --cpu_count -1
+python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means --alpha 0.40 --k 100 --train_size 0.8 --cpu_count -1
+python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means --alpha 0.30 --k 100 --train_size 0.8 --cpu_count -1
+python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means --alpha 0.20 --k 100 --train_size 0.8 --cpu_count -1
+python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means --alpha 0.10 --k 100 --train_size 0.8 --cpu_count -1
+python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means -- --alpha 1 --k 10000 --train_size 0.8 --cpu_count -1
+python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means --alpha 0.90 --k 10000 --train_size 0.8 --cpu_count -1
+python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means --alpha 0.80 --k 10000 --train_size 0.8 --cpu_count -1
+python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means --alpha 0.70 --k 10000 --train_size 0.8 --cpu_count -1
+python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means --alpha 0.60 --k 10000 --train_size 0.8 --cpu_count -1
+python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means --alpha 0.50 --k 10000 --train_size 0.8 --cpu_count -1
+python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means --alpha 0.40 --k 10000 --train_size 0.8 --cpu_count -1
+python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means --alpha 0.30 --k 10000 --train_size 0.8 --cpu_count -1
+python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means --alpha 0.20 --k 10000 --train_size 0.8 --cpu_count -1
+python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means --alpha 0.10 --k 10000 --train_size 0.8 --cpu_count -1
+python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means -- --alpha 1 --k 100000 --train_size 0.8 --cpu_count -1
 python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means --alpha 0.90 --k 100000 --train_size 0.8 --cpu_count -1
 python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means --alpha 0.80 --k 100000 --train_size 0.8 --cpu_count -1
 python -m gym.training_etfstsm_moskowitz --utility MaxDD --functional means --alpha 0.70 --k 100000 --train_size 0.8 --cpu_count -1
