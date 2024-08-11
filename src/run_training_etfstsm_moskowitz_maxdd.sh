00305af07cc4fbd26d4f13e27253209093d49360
dcuoliveira <dan.cunhaa@gmail.com>
2024-08-11 13:17:55 -0300

    fix runs

--- src/run_training_etfstsm_moskowitz_maxdd.sh
+++ src/run_training_etfstsm_moskowitz_maxdd.sh
@@ -1,6 +1,6 @@
-python -m gym.training_alphas_etfstsm_moskowitz --utility MaxDD --functional means --alpha 0 --k 100 --train_size 0.8 --cpu_count -1
-python -m gym.training_alphas_etfstsm_moskowitz --utility MaxDD --functional means --alpha 0 --k 1000 --train_size 0.8 --cpu_count -1
-python -m gym.training_alphas_etfstsm_moskowitz --utility MaxDD --functional means --alpha 0 --k 10000 --train_size 0.8 --cpu_count -1
-python -m gym.training_alphas_etfstsm_moskowitz --utility MaxDD --functional means --alpha 0 --k 100000 --train_size 0.8 --cpu_count -1
+python -m gym.training_alphas_etfstsm_moskowitz --utility MaxDD --functional means --k 100 --train_size 0.8 --cpu_count -1
+python -m gym.training_alphas_etfstsm_moskowitz --utility MaxDD --functional means --k 1000 --train_size 0.8 --cpu_count -1
+python -m gym.training_alphas_etfstsm_moskowitz --utility MaxDD --functional means --k 10000 --train_size 0.8 --cpu_count -1
+python -m gym.training_alphas_etfstsm_moskowitz --utility MaxDD --functional means --k 100000 --train_size 0.8 --cpu_count -1
 
 
