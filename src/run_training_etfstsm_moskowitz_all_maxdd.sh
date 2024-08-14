4ef05c9974aa587653cf83c52c0e09773d12e0fc
dcuoliveira <dan.cunhaa@gmail.com>
2024-08-14 10:36:49 -0300

    first commit all etfs

---
+++ src/run_training_etfstsm_moskowitz_all_maxdd.sh
@@ -0,0 +1,7 @@
+python -m gym.training_alphas_etfstsm_moskowitz_all --utility MaxDD --functional means --k 10 --train_size 0.8 --cpu_count -1
+python -m gym.training_alphas_etfstsm_moskowitz_all --utility MaxDD --functional means --k 100 --train_size 0.8 --cpu_count -1
+python -m gym.training_alphas_etfstsm_moskowitz_all --utility MaxDD --functional means --k 1000 --train_size 0.8 --cpu_count -1
+python -m gym.training_alphas_etfstsm_moskowitz_all --utility MaxDD --functional means --k 10000 --train_size 0.8 --cpu_count -1
+python -m gym.training_alphas_etfstsm_moskowitz_all --utility MaxDD --functional means --k 100000 --train_size 0.8 --cpu_count -1
+
+
---
+++ src/run_training_etfstsm_moskowitz_all_sortino.sh
@@ -0,0 +1,6 @@
+python -m gym.training_alphas_etfstsm_moskowitz_all --utility Sortino --functional means --k 10 --train_size 0.8 --cpu_count -1
+python -m gym.training_alphas_etfstsm_moskowitz_all --utility Sortino --functional means --k 100 --train_size 0.8 --cpu_count -1
+python -m gym.training_alphas_etfstsm_moskowitz_all --utility Sortino --functional means --k 1000 --train_size 0.8 --cpu_count -1
+python -m gym.training_alphas_etfstsm_moskowitz_all --utility Sortino --functional means --k 10000 --train_size 0.8 --cpu_count -1
+python -m gym.training_alphas_etfstsm_moskowitz_all --utility Sortino --functional means --k 100000 --train_size 0.8 --cpu_count -1
+
